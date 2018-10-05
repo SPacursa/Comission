e55b5e768015366f2f212207191db70f720c3658
Sean Pacursa <SPacursa2478@jh-mba158-14.d214.org>
2018-10-05 11:12:59 -0500

    Initial Commit

---
+++ Comission/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Comission
+//
+//  Created by Sean Pacursa on 10/5/18.
+//  Copyright © 2018 John Hersey Highschool. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
